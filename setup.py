2ab3b04d6a26f0c6f18d8e181a99da773b0ece3d
MathisJuretRafin <148776485+MathisJuretRafin@users.noreply.github.com>
2024-01-22 14:28:09 +0100

    Ajout fichier setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='JURET-RAFIN-CDOF1-Quiz',
+	version='1.0',
+	author='MathisJuretRafin',
+	licence='MIT',
+	long_description=open('README.md').read(),
+)
+
